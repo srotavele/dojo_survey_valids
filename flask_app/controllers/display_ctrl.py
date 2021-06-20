b88296c8aa328754ac669fcbd46fdca054cbcebe
MRea <rotavele666@pm.me>
2021-06-19 17:02:27 -0700

    second commit

--- flask_app/controllers/display_ctrl.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask, render_template, request, redirect, session
-from flask_app import app
-from ..models.display_mdl import Display
-from ..models.login_mdl import Login
--- flask_app/models/login_mdl.py
+++
@@ -1,3 +0,0 @@
-from ..config.mysqlconnection import connectToMySQL
-
-
